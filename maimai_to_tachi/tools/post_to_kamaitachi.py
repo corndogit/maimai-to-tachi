1c35ced3bfb0c5262ed93c640b8ec73bc86ef33f
corndogit <101812777+corndogit@users.noreply.github.com>
2025-02-21 20:48:42 +0000

    Move main.py to tools.post_to_kamaitachi.py

--- maimai_to_tachi/tools/post_to_kamaitachi.py
+++ maimai_to_tachi/tools/post_to_kamaitachi.py
@@ -1,5 +1,10 @@
 from maimai_to_tachi import spreadsheet, tachi
 
+# A script that when executed will:
+# - Get the spreadsheet from Google Sheets for the configured user
+# - Build and send a request object to Kamaitachi
+# - Optionally save a backup of scores to a JSON file
+
 if __name__ == "__main__":
     scores_from_sheet = spreadsheet.get_scores_from_maimai_spreadsheet()
     scores_request = tachi.add_scores_to_request_body(scores_from_sheet)
