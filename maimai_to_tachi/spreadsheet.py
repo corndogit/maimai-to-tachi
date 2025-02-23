08408591da6912007107c65d956d420c424b6949
corndogit <101812777+corndogit@users.noreply.github.com>
2025-02-23 17:45:40 +0000

    Add handling for Tachi code parsing errors

--- maimai_to_tachi/spreadsheet.py
+++ maimai_to_tachi/spreadsheet.py
@@ -1,5 +1,5 @@
 import json
-from typing import Iterable
+from typing import Iterable, Any
 from dacite import from_dict
 
 import gspread
@@ -21,16 +21,33 @@ def _read_scores_from_sheet(sheet: Spreadsheet) -> list[Score]:
     return scores
 
 
+def _parse_tachi_code(code: Iterable) -> Any:
+    try:
+        return json.loads(code)
+    except json.JSONDecodeError:
+        raise ValueError(f"Failed to parse the following Tachi code: {code}")
+
+
 def _map_and_append_scores(
         scores: list[Score],
         tachi_codes: list[Iterable]
 ) -> list[Score]:
+    ignored_codes = []
     for t in tachi_codes:
-        if t != []:
-            score = from_dict(data_class=Score, data=json.loads(t[0].rstrip(',')))
+        if t is not None and t != []:
+            code = t[0].rstrip(',')
+            try:
+                tachi_code_object = _parse_tachi_code(code)
+            except ValueError:
+                logger.warning(f"Could not parse code, skipping: {code}")
+                ignored_codes.append(code)
+            score = from_dict(data_class=Score,
+                              data=tachi_code_object)
             if score.judgements.miss > 0:
                 score.lamp = "CLEAR"
             scores.append(score)
+    if len(ignored_codes) > 0:
+        logger.warning(f"{len(ignored_codes)} score(s) skipped due to parsing errors")
 
 
 def get_scores_from_maimai_spreadsheet() -> list[Score]:
