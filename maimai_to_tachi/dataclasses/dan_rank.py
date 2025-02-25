b5611e5dba354316307e2bef75d9514d909bd5a4
corndogit <101812777+corndogit@users.noreply.github.com>
2025-02-25 22:42:55 +0000

    Use correct dan rank names for tachi

--- maimai_to_tachi/dataclasses/dan_rank.py
+++ maimai_to_tachi/dataclasses/dan_rank.py
@@ -22,30 +22,30 @@ class DanRank:
 
 
 dan_ranks = [
-    DanRank("1_DAN",      "1st Dan",      "C2"),
-    DanRank("2_DAN",      "2nd Dan",      "I2"),
-    DanRank("3_DAN",      "3rd Dan",      "O2"),
-    DanRank("4_DAN",      "4th Dan",      "C9"),
-    DanRank("5_DAN",      "5th Dan",      "I9"),
-    DanRank("6_DAN",      "6th Dan",      "O9"),
-    DanRank("7_DAN",      "7th Dan",      "C16"),
-    DanRank("8_DAN",      "8th Dan",      "I16"),
-    DanRank("9_DAN",      "9th Dan",      "O16"),
-    DanRank("10_DAN",     "10th Dan",     "C23"),
+    DanRank("DAN_1",      "1st Dan",      "C2"),
+    DanRank("DAN_2",      "2nd Dan",      "I2"),
+    DanRank("DAN_3",      "3rd Dan",      "O2"),
+    DanRank("DAN_4",      "4th Dan",      "C9"),
+    DanRank("DAN_5",      "5th Dan",      "I9"),
+    DanRank("DAN_6",      "6th Dan",      "O9"),
+    DanRank("DAN_7",      "7th Dan",      "C16"),
+    DanRank("DAN_8",      "8th Dan",      "I16"),
+    DanRank("DAN_9",      "9th Dan",      "O16"),
+    DanRank("DAN_10",     "10th Dan",     "C23"),
     DanRank("KAIDEN",     "Grade MG",     "I23"),
 ]
 
 
-shindan_ranks = [
-    DanRank("1_SHINDAN",  "1st Shindan",  "C31"),
-    DanRank("2_SHINDAN",  "2nd Shindan",  "I31"),
-    DanRank("3_SHINDAN",  "3rd Shindan",  "O31"),
-    DanRank("4_SHINDAN",  "4th Shindan",  "C38"),
-    DanRank("5_SHINDAN",  "5th Shindan",  "I38"),
-    DanRank("6_SHINDAN",  "6th Shindan",  "O38"),
-    DanRank("7_SHINDAN",  "7th Shindan",  "C45"),
-    DanRank("8_SHINDAN",  "8th Shindan",  "I45"),
-    DanRank("9_SHINDAN",  "9th Shindan",  "O45"),
-    DanRank("10_SHINDAN", "10th Shindan", "C52"),
+shindan_ranks = [  # unsupported but here for completion
+    DanRank("SHINDAN_1",  "1st Shindan",  "C31"),
+    DanRank("SHINDAN_2",  "2nd Shindan",  "I31"),
+    DanRank("SHINDAN_3",  "3rd Shindan",  "O31"),
+    DanRank("SHINDAN_4",  "4th Shindan",  "C38"),
+    DanRank("SHINDAN_5",  "5th Shindan",  "I38"),
+    DanRank("SHINDAN_6",  "6th Shindan",  "O38"),
+    DanRank("SHINDAN_7",  "7th Shindan",  "C45"),
+    DanRank("SHINDAN_8",  "8th Shindan",  "I45"),
+    DanRank("SHINDAN_9",  "9th Shindan",  "O45"),
+    DanRank("SHINDAN_10", "10th Shindan", "C52"),
     DanRank("SHINKAIDEN", "Shindan MG",   "I52"),
 ]
